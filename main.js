385690bf95054b7163cb4178cd2622aa559a9a3f
Simonedambra <simonedambra130102@gmail.com>
2024-05-09 17:24:42 +0200

    upd

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 'use strict'
 
-const clicknumeri = document.getElementsByTagName('button.numeri')
+const clicknumeri = document.querySelectorAll('button.numeri')
 
 console.log(clicknumeri);
+for (let i = 0; i < clicknumeri.length; i++) {
+    clicknumeri[i].addEventListener('click', function () {
+        let result = document.getElementById('result');
+        let num = [parseInt(clicknumeri[i].innerText)];
+        parseInt(clicknumeri[i].innerText);
+        result.innerHTML = ['']
+        parseInt(result)
+        result.innerHTML += [num]
+
+
+
+        console.log(num)
+
+    });
+
+}
+
+
+
+
+
+
+
