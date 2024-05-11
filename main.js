9497c41c94e7725bbe947d83bdac38ccb24b5059
Simonedambra <simonedambra130102@gmail.com>
2024-05-11 18:57:58 +0200

    null

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 'use strict'
-let risultato = document.querySelector('#result')
-let somma = 0;
-const buttonrow = document.querySelector('.numbers')
-buttonrow.addEventListener('click', function (e) {
 
-    let valore = (e.target.innerHTML);
-    let numeri = parseInt(valore)
-    somma += numeri;
+const clicknumeri = document.querySelector('.numbers')
+const clickplus = document.getElementById('add')
+clicknumeri.addEventListener('click', function (e) {
+    let num1 = []
+    let num2 = [];
+    let aggiunta1 = 0;
+    let num = (e.target);
+    let numValue = num.innerText;
 
-    console.log(somma)
-    risultato.innerText = (somma)
-})
+    if (e.target.className !== 'numeri' && e.target.className !== 'large') {
+        console.log('hai cliccato non su un bottone')
+
+    } else if (e.target.className === 'numeri') {
+
+        let numer = '';
+        let numerotot = numer += numValue;
 
 
+        console.log('è numeri');
+    } else if (e.target.className === 'large') {
+
+        aggiunta1 = num1 += numValue;
+        console.log('è numeri')
+
+    }
+    console.log(numerotot);
+    console.log(num1);
+});
 
 
